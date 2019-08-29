8fea1723d47167b268f7330808d09edd02f5e07b
caspianzx <caspianzx@gmail.com>
2019-08-29 14:18:15 +0800

    completed everything except last furthers

--- script.jsx
+++ script.jsx
@@ -8,22 +8,47 @@ class List extends React.Component {
     }
   }
 
-  addItem(){
-    debugger;
+    addItem(){
+        let addItem = this.state.list;
+        let newWord = this.state.word
+        addItem.push(newWord);
+        console.log(addItem);
+        this.setState({list: addItem, word: " ",});
   }
 
-  changeHandler(){
-    debugger;
+    clearList () {
+        let clearItem = this.state.list;
+        clearItem = [];
+        this.setState({list: clearItem, word: " ",});
+
+
   }
+
+  changeHandler(event){
+    if (event.target.value.length == 6) {
+        alert("please key in less than 4 characters");
+
+    } else {
+        console.log(event.target.value);
+        let newWord = this.state.word;
+        newWord = event.target.value;
+        this.setState({word : newWord});
+    }
+
+  }
+
 
   render() {
       // render the list with a map() here
-
       console.log("rendering");
       return (
         <div className="list">
-          <input onChange={()=>{this.changeHandler()}} value={this.state.word}/>
+        <h1>TO DO ADD FUNCTION</h1>
+          <input onChange={(event)=>{this.changeHandler(event)}} value={this.state.word} maxLength ="4"/>
           <button onClick={()=>{this.addItem()}}>add item</button>
+          <button onClick={()=>{this.clearList()}}>clear item list</button>
+          <h2>To do list </h2>
+          {this.state.list.join(" ")}
         </div>
       );
   }
@@ -33,4 +58,3 @@ ReactDOM.render(
     <List/>,
     document.getElementById('root')
 );
-
